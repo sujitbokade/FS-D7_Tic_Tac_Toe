3c6c635e5f229ca9ee2229076dc22f4da4de53e2
sujit <sujitbokade2014@gmail.com>
2022-07-14 15:01:47 +0530

    Ability to Choose Letter

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -1,14 +1,28 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToe {
 
     static char[] board =new char[10];
+    static  Scanner sc=new Scanner(System.in);
+    static char playerLetter;
+    static char computerLetter;
     static void createBoard(char[]board){
         for(int i=1;i<10;i++){
             board[i]=' ';
         }
     }
+    static void chooseLetter(){
+        System.out.println("Choose any Letter (x,o)");
+        playerLetter=sc.next().charAt(0);
+        computerLetter=(playerLetter=='x')?'o':'x';
+        System.out.println("Your Letter is "+playerLetter);
+        System.out.println("Computer Letter is "+computerLetter);
+
+    }
     public static void main(String[] args) {
         createBoard(board);
+        chooseLetter();
     }
 }
